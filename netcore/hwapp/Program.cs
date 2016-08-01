7c1ce54e35084955304c74fd19d1ced0f63a0bfd
Luke Chen <Hsf3.luke.chen@augmentum.com>
2016-08-01 21:23:55 +0800

    [Tutorial] Add dotnet core environment in centos

---
+++ netcore/hwapp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Show("Give me a time");
+        }
+
+        public static void Show(string value) {
+            Console.WriteLine(value);
+        }
+    }
+}
