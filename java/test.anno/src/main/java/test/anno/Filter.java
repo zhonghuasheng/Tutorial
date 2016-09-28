e344a5f76988ad7b8bc03e61419c7c3333d4faad
luke.chen <xiaoyong6906@126.com>
2016-09-28 22:18:51 +0800

    [Java] Add annotation demo

---
+++ java/test.anno/src/main/java/test/anno/Filter.java
@@ -0,0 +1,69 @@
+package test.anno;
+
+@Table("user")
+public class Filter {
+
+    @Column("id")
+    private int id;
+
+    @Column("userName")
+    private String userName;
+
+    @Column("nickName")
+    private String nickName;
+
+    @Column("age")
+    private int age;
+
+    @Column("city")
+    private String city;
+
+    @Column("email")
+    private String email;
+
+    @Column("mobile")
+    private String mobile;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getNickName() {
+        return nickName;
+    }
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getMobile() {
+        return mobile;
+    }
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
